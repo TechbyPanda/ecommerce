509b33153e5e126a420cc956f45c5f8e15a57248
26palash <95613328+26palash@users.noreply.github.com>
2022-04-13 12:32:24 +0530

    files

---
+++ backend/model/order.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const orderSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    address:{
+        type:String,
+        requried:true,
+    },
+    products:[{
+        size:{
+            type:String,
+            required:true,
+        },
+        color:{
+            type:String,
+            required:true
+        },
+        brand:{
+            type:String,
+            required:true
+        },
+        quantity:{
+            type:String,
+            required:true
+        },
+        price:{
+            type:number,
+            required:true
+        },
+        gender:{
+            type:String,
+            requried:true
+        }
+    }],
+    total:{
+        type:number,
+        required:true
+    }
+})
